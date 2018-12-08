090e4a23e9d7363dedd67a81897fbb991ca8ec91
mobydickens <steadmegan@gmail.com>
2018-12-08 15:17:33 -0700

    changes probably

--- src/App.js
+++ src/App.js
@@ -1,28 +1,16 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import routes from './routes.js';
 import {Link} from 'react-router-dom';
+import {HashRouter as Router, Switch, Route};
+import Home from './Home.jsx';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <Link to="/" >
-            <p>Route 1</p>
-          </Link>
-          <Link to="/2" >
-            <p>Route 2</p>
-          </Link>
-          <Link to="/3" >
-            <p>Route 3</p>
-          </Link>
-          { routes }
-      </div>
+      <Router>
+        <Route path='/' component={ Home }/>
+      </Router>
     );
   }
 }
